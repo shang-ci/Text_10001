9f7c843b7bc60b34f379e74a46ea8382bc118d25
shang-ci <3071492178@qq.com>
2025-03-24 21:19:42 +0800

    朱行和——清空所有内容

--- Assets/Scripts/Item/StatusEffect_Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class StatusEffect_Item : Item
-{
-    //改变时机——在打出卡牌时触发状态效果的状态卡可以在此处改变时机
-    public abstract void ChangeTime(CharacterBase character);
-    public abstract void ExecuteEffect(CharacterBase from, CharacterBase target);
-    public abstract void RemoveEffect(CharacterBase character);
-}
